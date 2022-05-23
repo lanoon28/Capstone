fcc3b6ade4fa890687ef8dfd7269e7b1796ccb63
Hwang-Myeong-Geon <lanoon28@gmail.com>
2022-05-23 13:55:40 +0900

    ui

--- test_ui.py
+++ test_ui.py
@@ -2,6 +2,7 @@ import os
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import matplotlib as mat
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -23,24 +24,28 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle('Comments Detector')
 
     def ok_btn_click(self):
         self.hide()                     # 메인윈도우 숨김
-        self.second = secondwindow()    #
+        self.url = self.textEdit.toPlainText()
+        self.second = secondwindow(self.url)    #
         self.second.exec()              # 두번째 창을 닫을 때 까지 기다림
         self.show()                     # 두번째 창을 닫으면 다시 첫 번째 창이 보여짐짐
 
 class secondwindow(QDialog,QWidget,form_secondwindow):
-    def __init__(self):
+    def __init__(self, url):
         super(secondwindow,self).__init__()
         self.initUi()
+        self.setWindowTitle('Result')
         self.show()
+        self.label.setText(url)         #test용
         data = pd.read_excel('total.xlsx', index_col=0)
 
         # 결측치 드랍
         data = data.dropna(axis=0)
 
-        print('data : ', len(data))
+        #print('data : ', len(data))
 
         good = data[data['react'] == '긍정']
         good_len = len(good)
@@ -58,10 +63,9 @@ class secondwindow(QDialog,QWidget,form_secondwindow):
 
         x = [1,2]
         y = [good_len,bad_len]
-        label = ['positive', 'negative']
+        label = ['Good', 'Bad']
         ax.barh(x,y,tick_label =label)
-
-        ax.set_title("positive negative ratio")
+        ax.set_title("Ratio of malicious comments")
 
         self.canvas.draw()
 
