0840708540ef69a57632930ad599961a1bf5ecb5
Hwang-Myeong-Geon <lanoon28@gmail.com>
2022-05-24 13:15:07 +0900

    backuo

--- deepLearning_origin.py
+++ deepLearning_origin.py
@@ -32,32 +32,34 @@ tfv = TfidfVectorizer(tokenizer=okt.morphs, ngram_range=(1,2), min_df=3, max_df=
 tfv.fit(train_x)
 tfv_train_x = tfv.transform(train_x)
 clf = LogisticRegression(random_state=0)
-params = {'C': [15, 18, 19, 20, 22]}
-grid_cv = GridSearchCV(clf, param_grid=params, cv=3, scoring='accuracy', verbose=1)
-grid_cv.fit(tfv_train_x, train_y)
-print(grid_cv.best_params_, grid_cv.best_score_)
+# params = {'C': [15, 18, 19, 20, 22]}
+# grid_cv = GridSearchCV(clf, param_grid=params, cv=3, scoring='accuracy', verbose=1)
+# grid_cv.fit(tfv_train_x, train_y)
+# print(grid_cv.best_params_, grid_cv.best_score_)
+#
+# joblib.dump(grid_cv,'./newDeep.pkl')
 
-joblib.dump(grid_cv,'./newDeep.pkl')
+load_model = joblib.load('newDeep.pkl')
 
 tfv_test_x = tfv.transform(test_x)
-test_predict = grid_cv.best_estimator_.predict(tfv_test_x)
+test_predict = load_model.best_estimator_.predict(tfv_test_x)
 
 
-input_text = 'ㅅㅂ 이걸 왜 만드냐'
+# input_text = 'ㅅㅂ 이걸 왜 만드냐'
+#
+# input_text = re.compile(r'[ㄱ-ㅣ가-힣]+').findall(input_text)
+# input_text = [" ".join(input_text)]
+#
+# st_tfidf = tfv.transform(input_text)
 
-input_text = re.compile(r'[ㄱ-ㅣ가-힣]+').findall(input_text)
-input_text = [" ".join(input_text)]
+#st_predict = load_model.best_estimator_.predict(st_tfidf)
 
-st_tfidf = tfv.transform(input_text)
-
-st_predict = grid_cv.best_estimator_.predict(st_tfidf)
-
-print('감성 분류 모델의 정확도 : ',round(accuracy_score(test_y, test_predict), 3))
-
-if(st_predict == 0):
-    print('예측 결과: ->> 부정 감성')
-else :
-    print('예측 결과: ->> 긍정 감성')
+# print('감성 분류 모델의 정확도 : ',round(accuracy_score(test_y, test_predict), 3))
+#
+# if(st_predict == 0):
+#     print('예측 결과: ->> 부정 감성')
+# else :
+#     print('예측 결과: ->> 긍정 감성')
 
 test_input = pd.read_excel('44.xlsx')
 
@@ -67,7 +69,7 @@ test_df['Comment'] = test_df['Comment'].apply(lambda x: re.sub(r'[^ ㄱ-ㅣ 가-
 text_r = test_df['Comment']
 test = tfv.transform(text_r)
 
-ts_predict = grid_cv.best_estimator_.predict(test)
+ts_predict = load_model.best_estimator_.predict(test)
 
 
 
